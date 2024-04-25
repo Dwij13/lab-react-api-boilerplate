95b755b2f38c7a726d7ed5bdb04852e609584e6e
Dwij13 <dwijjindal2005@gmail.com>
2024-04-25 15:33:00 +0530

    Complete

---
+++ react-api/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css'
+import Books from './Components/Books';
+
+const App = () => {
+  return (
+    <div>
+      <Books />
+    </div>
+  );
+};
+
+export default App;
